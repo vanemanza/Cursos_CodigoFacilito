b76467c4d4c3459f8f2fb58faf7b186cf255a851
VaneManzanelli <vanemanza1@gmail.com>
2021-11-25 21:24:48 -0300

    Configuracion de static files

--- DJANGO/Proyecto/store/views.py
+++ DJANGO/Proyecto/store/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html', {
-        'mensaje' : 'Nuevo mensaje desde la vista',
-        'titulo' : 'index',
-    })
+    contexto = {
+        'mensaje' : 'Listado de Productos ',
+        'titulo' : 'Productos',
+        'productos': [
+            {'titulo': 'Playera', 'precio': 5, 'stock': True},
+            {'titulo': 'Camisa', 'precio': 7, 'stock': True},
+            {'titulo': 'Mochila', 'precio': 20, 'stock': False}, #producto
+            {'titulo': 'Laptop', 'precio': 200, 'stock': True}, #producto
+
+        ]
+    }
+    return render(request, 'index.html', contexto)
+
