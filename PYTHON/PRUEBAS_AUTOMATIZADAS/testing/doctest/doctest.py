d6e6495b7219c3780ca3eda5db2471075c59b474
VaneManzanelli <vanemanza1@gmail.com>
2021-11-29 17:30:32 -0300

    reorganización de directorios

---
+++ PYTHON/PRUEBAS_AUTOMATIZADAS/testing/doctest/doctest.py
@@ -0,0 +1,16 @@
+"""
+no tenemos q instalar nada
+creo un archivo con extencion txt
+x ej--> test.txt
+"""
+
+def suma(a,b):
+    """La suma de 2 + 2 es igual a 4
+    >>> suma(2,2)
+    4
+    """
+    return a+b
+
+#import doctest
+#doctest.testmod()
+
