25a2f157377ab87cefef6111cdd40d167e399816
VaneManzanelli <vanemanza1@gmail.com>
2021-12-07 08:41:23 -0300

    First commit

---
+++ DJANGO/Proyecto2/adopcion/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    
+    nombre = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+    info = models.TextField()    
+    foto = models.ImageField(upload_to="mascota_perdida", null=True, blank=True )
