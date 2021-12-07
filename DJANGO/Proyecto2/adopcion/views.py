3d18a1794c5e317acccc03826fa83b7f263e1717
VaneManzanelli <vanemanza1@gmail.com>
2021-12-07 09:39:19 -0300

    templates base

--- DJANGO/Proyecto2/adopcion/views.py
+++ DJANGO/Proyecto2/adopcion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def adopcion(request):
+    mensaje = "hola persona q adopta"
+    return HttpResponse(mensaje)
