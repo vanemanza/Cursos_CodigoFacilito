9997592114de017a4c60b15c669a606d5721be24
VaneManzanelli <vanemanza1@gmail.com>
2021-12-07 11:31:56 -0300

    navbar template y settings

--- DJANGO/Proyecto2/mascota/urls.py
+++ DJANGO/Proyecto2/mascota/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('form', views.form_alta, name="form_alta"),
 ]
--- DJANGO/Proyecto2/mascota/views.py
+++ DJANGO/Proyecto2/mascota/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
 
     return render(request, 'mascota/index.html', {})
+
+def form_alta(request):
+
+    return render(request, 'mascota/mascota_form.html', {})    
+
