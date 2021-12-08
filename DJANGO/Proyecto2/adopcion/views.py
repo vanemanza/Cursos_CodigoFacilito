5e078ab5e8b5843db1fadc5d00bea65b3c4f0df1
VaneManzanelli <vanemanza1@gmail.com>
2021-12-08 17:30:40 -0300

    eliminé el proyecto Refugio

--- DJANGO/Proyecto2/adopcion/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def adopcion(request):
-    mensaje = "hola persona q adopta"
-    return HttpResponse(mensaje)
--- DJANGO/Proyecto2/mascota/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('nuevo', views.mascota, name="nuevo"),
-    path('listar', views.listado, name="listar"),
-]
