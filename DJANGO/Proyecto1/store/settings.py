fde7fbf52136a01ce85a332471ee799c4b9c9fb6
VaneManzanelli <vanemanza1@gmail.com>
2021-12-09 21:49:20 -0300

    Modifiqué settings

--- DJANGO/Proyecto1/store/settings.py
+++ DJANGO/Proyecto1/store/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Cordoba'
 
 USE_I18N = True
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+sSTATIC_URL = '/static/'
 
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static'),
