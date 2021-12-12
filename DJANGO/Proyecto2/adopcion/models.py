5a6c93d0f20d857ab6923ed0ed339ab94150ae74
VaneManzanelli <vanemanza1@gmail.com>
2021-12-12 10:56:03 -0300

    modelos y migraciones

--- DJANGO/Proyecto2/adopcion/models.py
+++ DJANGO/Proyecto2/adopcion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=15) 
+    email = models.EmailField()
+    domiclio = models.TextField()
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
