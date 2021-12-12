c819b74db71a8eda6e0527a0ab5de35bc83bc0c4
VaneManzanelli <vanemanza1@gmail.com>
2021-12-12 10:42:27 -0300

    modifiqué apps

--- DJANGO/Proyecto2/refugio/settings.py
+++ DJANGO/Proyecto2/refugio/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.adopcion',
-    'apps.mascota',
+    'adopcion',
+    'mascota',
+    
 ]
 
 MIDDLEWARE = [
