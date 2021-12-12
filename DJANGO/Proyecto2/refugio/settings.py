17f6eb92b8659ca42e43471bc943e1d20bad3c48
VaneManzanelli <vanemanza1@gmail.com>
2021-12-12 10:15:25 -0300

    config de postgres y migrate

--- DJANGO/Proyecto2/refugio/settings.py
+++ DJANGO/Proyecto2/refugio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'refugio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'refugio',
+        'USER': 'vane',
+        'PASSWORD': 'vane',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
