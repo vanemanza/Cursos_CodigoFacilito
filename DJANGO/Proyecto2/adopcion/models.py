3a36fc5c5b516a530359e929289b653e0c268286
VaneManzanelli <vanemanza1@gmail.com>
2021-12-12 22:59:09 -0300

    modelos y migraciones

--- DJANGO/Proyecto2/adopcion/models.py
+++ DJANGO/Proyecto2/adopcion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=70)
+    edad = models.IntegerField() 
+    telefono = models.CharField(max_length=12)
+    email = models.EmailField()  
+    domicilio = models.TextField()  
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
--- DJANGO/Proyecto2/mascota/models.py
+++ DJANGO/Proyecto2/mascota/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from adopcion.models import Persona 
 # Create your models here.
+
+class Vacuna(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre 
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=50)  
+    sexo = models.CharField(max_length=10)  
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField() 
+    persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
+    vacuna = models.ManyToManyField(Vacuna)
